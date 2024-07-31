da06103ed2ac3b617cfd37760484355056a8e5ee
itarsdev <dev@itars.ru>
2024-07-31 20:54:14 +0500

    ferst massage english

--- src/tbot.js
+++ src/tbot.js
@@ -11,13 +11,13 @@ bot.start((ctx) => {
     const user = ctx.message.from;
     const userName = user.username ? `@${user.username}` : user.first_name; 
 
-    ctx.replyWithMarkdownV2(`*Hey, ${userName}✌ \n\nWelcome to Catacombs*`, {
+    ctx.replyWithMarkdownV2(`*Hey, ${userName}✌ \n\nWelcome to Catacombs\\!*\n\nA whole world of adventures is available to you\\.\nDon't forget to invite your friends\\, the path to success will be faster in a team\\.\n\nThe adventure begins 🚀`, {
         reply_markup: {
             inline_keyboard: [
-                [{ text: "👋 Start now", web_app: {url: WEB_LINK} }],
                 [{ text: "✨ Visit website", web_app: {url: WEB_LINK} }],
                 [{ text: "✅ Subscribe channel", web_app: {url: WEB_LINK} }],
-                [{ text: "❓ Help", web_app: {url: WEB_LINK} }]
+                [{ text: "❓ Help", web_app: {url: WEB_LINK} }],
+                [{ text: "👋 Start now", web_app: {url: WEB_LINK} }]
             ],
         },
     });
